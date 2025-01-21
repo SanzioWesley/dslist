8c1f3e0a2e7183451154169b317157dcd27d2c8b
Sanzio Wesley <sanziowesley@hotmail.com>
2025-01-21 20:00:58 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -24,13 +24,11 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	
-			
-    //@Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")
 	private String shortDescription;
-
-	//@Column(columnDefinition = "TEXT")
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
+	
 	
     public Game() {
     }
