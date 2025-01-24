2452c07f0ea2f019442ab8792a263855ababb55d
Sanzio Wesley <sanziowesley@hotmail.com>
2025-01-24 20:53:24 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -17,24 +17,24 @@ public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
 		return dto;
-		//return      new GameDTO(result);
+		// return new GameDTO(result);
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<GameMinDTO> findAll(){
-		List<Game> result = gameRepository.findAll(); 
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
-	
+
 	@Transactional(readOnly = true)
-	public List<GameMinDTO> findByList(Long listId){
+	public List<GameMinDTO> findByList(Long listId) {
 		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
